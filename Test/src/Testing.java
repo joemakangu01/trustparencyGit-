628a925bc291dcaf60d1b3f0b405befbb72ab67f
joemakangu01 <joe@trustparency.es>
2012-08-03 12:37:54 +0200

    Uploading Test project

---
+++ Test/src/Testing.java
@@ -0,0 +1,13 @@
+		
+public class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola El Mundo");
+
+	}
+
+}
